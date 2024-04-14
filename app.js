1083d7961a2bf0f562ecaea62ab0fcca71094472
lina-bkd <96200016+lina-bkd@users.noreply.github.com>
2024-04-14 18:06:38 +0200

    add validation schemas

--- app.js
+++ app.js
@@ -3,38 +3,36 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const io = require('socket.io')(server);
+const validateMessage = require('./validateMessageMiddleware'); // Importez le middleware de validation
 
 app.get('/', (req, res) => {
-  res.send('<h1>Resteau Server</h1>');
+    res.send('<h1>Resteau Server</h1>');
 });
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
 
 io.on('connect', (socket) => {
-  console.log('Connected with ID', socket.id);
+    console.log('Connected with ID', socket.id);
   
-  socket.on('new-message', (message) => {
-    const { client, content } = JSON.parse(message);
+    socket.on('new-message', (message) => {
+        console.log('New message:', message);
+        // Utilisez le middleware de validation ici
+        validateMessage(message, () => {
+            const { client, content, type } = JSON.parse(message);
+            console.log('Client:', client);
+            console.log('Content:', content);
+            console.log('Type:', type);
 
-    // Any validation can be done here on content
-
-    io.emit('broadcast', {
-      client,
-      content : content + ' - Server' // Adding this to show that server is responding
+            io.emit('broadcast', {
+                client,
+                content: content + ' - Server'
+            });
+        });
     });
-  });
 
-  // If you want to create a new type of events
-  // socket.onAny((eventName, ...args) => {
-  //   console.log(`Received event: ${eventName}`, args);  
-  // }); 
-
-  socket.on('disconnect', () => {
-    console.log('Socket Disconnected', socket.id);
-  });
+    socket.on('disconnect', () => {
+        console.log('Socket Disconnected', socket.id);
+    });
 });
-
-
-
