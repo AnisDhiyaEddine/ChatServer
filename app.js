0073551654f3bd86a0d857e95efaeda6cfc2a758
Maodk223 <59046954+Maodk223@users.noreply.github.com>
2024-04-21 22:17:06 +0200

    correct format transmition

--- app.js
+++ app.js
@@ -6,43 +6,47 @@ const io = require('socket.io')(server);
 const validateMessage = require('./validateMessageMiddleware'); // Importez le middleware de validation
 
 app.get('/', (req, res) => {
-    res.send('<h1>Resteau Server</h1>');
+  res.send('<h1>Resteau Server</h1>');
 });
 
 server.listen(3000, () => {
-    console.log('listening on *:3000');
+  console.log('listening on *:3000');
 });
 
 io.on('connect', (socket) => {
-    console.log('Connected with ID', socket.id);
-  
-    socket.on('new-message', (message) => {
-        console.log('New message:', message);
-        const { client, content, type } = JSON.parse(message);
-        
-        // Utilisez le middleware de validation ici
-        try {
-            validateMessage(message, () => {
-                
-                console.log('Client:', client);
-                console.log('Content:', content);
-                console.log('Type:', type);
+  console.log('Connected with ID', socket.id);
 
-                io.emit('broadcast', {
-                    client,
-                    content: content + ' - Server'
-                });
-            });
-        } catch (error) {
-            console.error('Validation error:', error.message);
-            io.emit('broadcast', {
-                client,
-                content: 'Validation error: ' + error.message
-            });
-        }
-    });
+  socket.on('new-message', (message) => {
+    console.log('New message:', message);
+    const { client, content, type } = JSON.parse(message);
 
-    socket.on('disconnect', () => {
-        console.log('Socket Disconnected', socket.id);
-    });
+    console.log('Client:', client);
+    console.log('Content:', content);
+    console.log('Type:', type);
+
+    // Utilisez le middleware de validation ici
+    try {
+      validateMessage(message, () => {
+        console.log('Validation passed', JSON.parse(content).content);
+        io.emit('broadcast', {
+          client,
+          content:
+            type == 'Text'
+              ? JSON.parse(content).content
+              : JSON.stringify(JSON.parse(content).content),
+          type,
+        });
+      });
+    } catch (error) {
+      console.error('Validation error:', error.message);
+      io.emit('broadcast', {
+        client,
+        content: 'Validation error: ' + error.message,
+      });
+    }
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Socket Disconnected', socket.id);
+  });
 });
